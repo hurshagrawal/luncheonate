da5bbfd32be8d8e296428f18539a7c28a2284319
Hursh Agrawal <h@hurshagrawal.com>
2013-05-12 19:49:48 -0400

    fixed user methods

--- src/luncheonate/models/user.clj
+++ src/luncheonate/models/user.clj
@@ -1,16 +1,18 @@
 (ns luncheonate.models.user
-  (:use korma.core))
+  (:use korma.core)
+  (:require [luncheonate.db.config :as config]))
 
 (defentity user
   (table :users)
+  (database config/db)
   (entity-fields :id :name))
 
 (defn authenticate [user password]
   (= (:password user) password))
 
 (defn find-by-email [email]
-  (select user
-    (where {:email email})))
+  (first (select user
+           (where {:email email}))))
 
 (defn create [params]
   (insert user
